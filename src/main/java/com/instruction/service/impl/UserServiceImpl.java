717ede2e6e00bee0b16685c17264237a888ff273
zhouchao <1137730824@qq.com>
2017-02-17 20:15:57 +0800

    成功部署到maxleap上

---
+++ src/main/java/com/instruction/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.instruction.service.impl;
+
+import com.instruction.dao.UserDao;
+import com.instruction.model.User;
+import com.instruction.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Created by Zhangxq on 2016/7/15.
+ */
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class UserServiceImpl implements UserService {
+    
+    @Resource
+    private UserDao userDao;
+
+    public User getUserById(Long userId) {
+        return userDao.selectUserById(userId);
+    }
+    
+    public User getUserByPhoneOrEmail(String emailOrPhone, Short state) {
+        return userDao.selectUserByPhoneOrEmail(emailOrPhone,state);
+    }
+    
+    public List<User> getAllUser() {
+        return userDao.selectAllUser();
+    }
+}
--- target/instructions/WEB-INF/views/index.jsp
+++
@@ -1,6 +0,0 @@
-<%@ page contentType="text/html;charset=utf-8" %>
-<html>
-<body>
-<h2>欢迎使用 软件说明书！</h2>
-</body>
-</html>
